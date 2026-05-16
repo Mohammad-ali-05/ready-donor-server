79653fe18549a55f29d0d151556bdab42354b669
Mohammad Ali <muhammadali.pass1@gmail.com>
2026-05-16 15:04:56 +0600

    Downloaded all the necessary libaraies. And created basic project structure.

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+app.use((req, res, next) => {
+    console.log(
+        `⚡ ${req.method} - ${req.path} from ${
+            req.host
+        } at ⌛ ${new Date().toLocaleString()}`,
+    );
+    next();
+});
+
+// mongodb
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        console.log("✅ Connected to MongoDB");
+        //DB
+        const db = client.db("readyDonor");
+        //Collections
+        const usersCollection = db.collection("user");
+
+        app.get("/", (req, res) => {
+            res.send("Hello from ready donor server");
+        });
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+// Listening to incoming request
+app.listen(port, () => {
+    console.log(`Listening of port ${port}`);
+});
